b6588dba3362fdd6054ea6540e3638e9dcfd510a
Agustin Bustos Piasentini <bustos-agustin@hotmail.com - agustin.bustos.p@gmail.com>
2023-02-12 17:17:16 -0300

    FIX

--- Consolidar Todos los PDF de Carpeta y subcarpetas.py
+++ Consolidar Todos los PDF de Carpeta y subcarpetas.py
@@ -21,18 +21,23 @@ pdfFiles.sort(key=os.path.basename)
  # Crear el objeto PDF merger 
 pdfMerger = PdfMerger() 
 
- # Añadir cada archivo PDF al objeto merger 
+#Contador de páginas para los marcadores de página
+Primer_página = 0
+
+# Añadir cada archivo PDF al objeto merger 
 for filename in pdfFiles: 
     with open(filename, 'rb') as f: 
+        #Crear un objeto PDF reader para cada archivo PDF
         pdf_reader = PdfReader(f)
+        #Obtener el número de páginas del archivo PDF
         Number_of_pages = len(pdf_reader.pages) -1
+        #Agregar un marcador de página para cada archivo PDF sin el path
         pdfMerger.append(pdf_reader , pages=(0, Number_of_pages + 1 ))
-
-for i in range(len(pdfFiles)):
-    #pdfMerger.add_outline_item(title=str(pdfFiles[i]), pagenum=i)
-    #Agregar un marcador de página para cada archivo PDF sin el path
-    pdfMerger.add_outline_item(title=str(os.path.basename(pdfFiles[i])), pagenum=i)
-
+        #Agregar un marcador de página para cada archivo PDF con el path
+        pdfMerger.add_outline_item(title=str(os.path.basename(filename)) , pagenum=Primer_página)
+        #Actualizar el contador de páginas
+        Primer_página += Number_of_pages +1
+    
  # Escribir el contenido del merger en un nuevo archivo PDF 								  
 with open('Consolidado IVA IIBB.pdf', 'wb') as f: 
     pdfMerger.write(f)
